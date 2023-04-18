f152e173ba0b058ad33eaeba099d08f561365f3a
Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>
2023-04-17 18:24:43 -0700

    Updated server.js
    updated secret

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: '93 93/93',
+  secret: 'secretsquirrel',
   cookie: {
         expires: 10 * 60 * 1000
   },
